c22e18d1d33e7ad3a92a60cb7d19148cab578c59
IAMHUDA <hudamiftahul362@gmail.com>
2023-03-30 12:14:48 +0700

    membuat prosedural input

--- quick_short/quick_short.cpp
+++ quick_short/quick_short.cpp
@@ -6,3 +6,26 @@ int arr[20];
 int cmp_count = 0; //number of comparation 
 int mov_count = 0; //number of data movement 
 int n;
+
+
+void input() {
+	while (true)
+	{
+		cout << "masukan panjang elemen array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+	cout << "\n==================" << endl;
+	cout << "membuat elemen array" << endl;
+	cout << "\n==================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
